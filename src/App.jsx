489853dea19d9b4facb2a949a20eca2db6074a6a
Fabio Encarnacion <fabio.encarnacion1@gmail.com>
2025-04-01 16:00:44 -0400

    Cambios en el header para incrementar y disminuir

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   const [data, setData] = useState(db)
   const [cart, setCart] = useState([])
 
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
   function addToCart(item){
 
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
@@ -29,12 +32,40 @@ function App() {
   }
   
 
+  function increaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) {
+        return{
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id){
+    const updatedCart = cart.map(item => {
+    if(item.id === id && item.quantity > MIN_ITEMS) {
+      return{
+        ...item,
+        quantity: item.quantity - 1
+      }
+    }
+    return item
+  })
+  setCart(updatedCart)
+}
+
   return (
     <>
 
       <Header 
         cart={cart}
         removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreasyQuantity={decreaseQuantity}
       />
       
     <main className="container-xl mt-5">
